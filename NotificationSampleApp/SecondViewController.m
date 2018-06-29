131c7951890a4643ac8cc215d068020adc4dce18
Srinivasan Sampath <sri.persnl@gmail.com>
2018-06-29 18:55:25 +0530

    Update SecondViewController.m

--- NotificationSampleApp/SecondViewController.m
+++ NotificationSampleApp/SecondViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "SecondViewController.h"
+#import "Utility.h"
 
 @interface SecondViewController ()
 
@@ -16,22 +17,22 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+   
+    [self.navigationController setNavigationBarHidden:YES];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+   
+    
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)secondPageButtonAction:(id)sender {
+    
+    [[NSNotificationCenter defaultCenter]postNotificationName:@"str" object:nil];
+    
+    ViewController *viewController = [[Utility sharedInstance]viewControllerObject];
+    [self.navigationController popViewControllerAnimated:YES];
+    NSLog(@"%@",viewController);
 }
-*/
-
 @end
